676e05af0406ead3b2b37614d91231215869a2e1
ap0calyp <>
2022-01-18 18:47:57 -0500

    reduce deployment complexity, add collateral removed

--- src/cauldron.ts
+++ src/cauldron.ts
@@ -8,10 +8,21 @@ import {
   LogRemoveCollateral,
   LogRepay,
   LogWithdrawFees,
-} from '../generated/cauldron/cauldron'
+} from '../generated/templates/cauldron/cauldron'
+import {
+  degenbox
+} from '../generated/degenbox/degenbox'
 import { CauldronFee, ExchangeRate, UserLiquidation } from '../generated/schema'
-import { erc20 } from '../generated/templates/StandardToken/erc20'
+import { erc20 } from '../generated/templates/cauldron/erc20'
 import { cauldronMediumRiskV1 } from '../generated/templates/cauldron/cauldronMediumRiskV1'
+
+const LIQUIDATE_SIGNATURES = [
+    '0x912860c5' // liquidate(address[],uint256[],address,address)
+]
+
+function decimals(exp: number): BigDecimal {
+  return BigInt.fromI32(10).pow(u8(exp)).toBigDecimal()
+}
 
 const EIGHTEEN_DECIMALS = BigInt.fromI32(10).pow(18).toBigDecimal()
 
@@ -22,17 +33,18 @@ function getCauldronFee(address: Address): CauldronFee {
     entity.totalBorrowElastic = BigDecimal.zero()
     entity.accrueInfoFeesEarned = BigDecimal.zero()
     entity.accrueInfoFeesWithdrawn = BigDecimal.zero()
-    let cauldron = cauldronContract.bind(address)
+    const cauldron = cauldronContract.bind(address)
     entity.masterContract = cauldron.masterContract().toHex()
     entity.bentoBox = cauldron.bentoBox().toHex()
-    let token = erc20.bind(cauldron.collateral())
-    entity.symbol = token.symbol()
+    const token = erc20.bind(cauldron.collateral())
+    entity.collateralSymbol = token.symbol()
+    entity.collateralName = token.name()
   }
   return entity
 }
 
 function getUserLiquidation(user: string, txHash: string): UserLiquidation {
-  let id = user + '-' + txHash
+  const id = user + '-' + txHash
   let userLiquidation = UserLiquidation.load(id)
   if (!userLiquidation) {
     userLiquidation = new UserLiquidation(id)
@@ -40,6 +52,7 @@ function getUserLiquidation(user: string, txHash: string): UserLiquidation {
     userLiquidation.transaction = txHash
     userLiquidation.collateralRemoved = BigDecimal.zero()
     userLiquidation.loanRepaid = BigDecimal.zero()
+    userLiquidation.direct = false
   }
   return userLiquidation
 }
@@ -54,50 +67,52 @@ function getExchangeRate(cauldron: string): ExchangeRate {
 
 
 export function handleLogAccrue(event: LogAccrue): void {
-  let cauldronFee = getCauldronFee(event.address)
-  let eventAccruedAmount = event.params.accruedAmount.divDecimal(EIGHTEEN_DECIMALS)
+  const cauldronFee = getCauldronFee(event.address)
+  const eventAccruedAmount = event.params.accruedAmount.divDecimal(EIGHTEEN_DECIMALS)
   cauldronFee.accrueInfoFeesEarned = cauldronFee.accrueInfoFeesEarned.plus(eventAccruedAmount)
   cauldronFee.totalBorrowElastic = cauldronFee.totalBorrowElastic.plus(eventAccruedAmount)
   cauldronFee.save()
 }
 
 export function handleLogBorrow(event: LogBorrow): void {
-  let cauldronFee = getCauldronFee(event.address)
-  let eventBorrowedAmount = event.params.amount.divDecimal(EIGHTEEN_DECIMALS)
+  const cauldronFee = getCauldronFee(event.address)
+  const eventBorrowedAmount = event.params.amount.divDecimal(EIGHTEEN_DECIMALS)
   cauldronFee.totalBorrowElastic = cauldronFee.totalBorrowElastic.plus(eventBorrowedAmount)
 
   // oldCauldrons have only 2 pieces of data in accrueInfo, new cauldrons have 3
-  let oldCauldrons = [
+  const oldCauldrons = [
       '0x469a991a6bb8cbbfee42e7ab846edeef1bc0b3d3'.toUpperCase(),// CauldronLowRiskV1
       '0x4a9cb5d0b755275fd188f87c0a8df531b0c7c7d2'.toUpperCase(),// CauldronMediumRiskV1
+      '0x4c56DbCC056655b8813539aF9C819ae128c07e17'.toUpperCase() // CauldronMediumRiskV1
   ]
   // Borrowing while loan is active sort of throws this off and I can't figure out why.  So I take the direct value from contract
   //  if this feeAmount was a separate variable then I could track the fee independently of the amount
   //   emit LogBorrow(msg.sender, to, amount.add(feeAmount), part);
   //
   if (oldCauldrons.indexOf(cauldronFee.masterContract.toUpperCase()) > -1) {
-    let deployedCauldron = cauldronMediumRiskV1.bind(event.address)
+    const deployedCauldron = cauldronMediumRiskV1.bind(event.address)
     cauldronFee.accrueInfoFeesEarned = deployedCauldron.accrueInfo().value1.toBigDecimal().div(EIGHTEEN_DECIMALS)
   } else {
-    let deployedCauldron = cauldronContract.bind(event.address)
+    const deployedCauldron = cauldronContract.bind(event.address)
     cauldronFee.accrueInfoFeesEarned = deployedCauldron.accrueInfo().value1.toBigDecimal().div(EIGHTEEN_DECIMALS)
   }
   cauldronFee.save()
 }
 
 export function handleLogRepay(event: LogRepay): void {
-  let cauldronFee = getCauldronFee(event.address)
-  let eventRepayPart = event.params.amount.divDecimal(EIGHTEEN_DECIMALS)
+  const cauldronFee = getCauldronFee(event.address)
+  const eventRepayPart = event.params.amount.divDecimal(EIGHTEEN_DECIMALS)
   cauldronFee.totalBorrowElastic = cauldronFee.totalBorrowElastic.minus(eventRepayPart)
   cauldronFee.save()
 
   // liquidation handler
-  let invoker = event.transaction.from.toHex().toLowerCase()
-  let user = event.params.to.toHex().toLowerCase()
+  const invoker = event.transaction.from.toHex().toLowerCase()
+  const user = event.params.to.toHex().toLowerCase()
   if (invoker != user) {
-    let txHash = event.transaction.hash.toHex()
-    let userLiquidation = getUserLiquidation(user, txHash)
-    let exchangeRate = getExchangeRate(event.address.toHex())
+    const txHash = event.transaction.hash.toHex()
+    const userLiquidation = getUserLiquidation(user, txHash)
+    const exchangeRate = getExchangeRate(event.address.toHex())
+    userLiquidation.direct = LIQUIDATE_SIGNATURES[0] == event.transaction.input.toHex().slice(0, 10).toLowerCase()
     userLiquidation.timestamp = event.block.timestamp
     userLiquidation.loanRepaid = event.params.amount.divDecimal(EIGHTEEN_DECIMALS)
     userLiquidation.exchangeRate = exchangeRate.rate
@@ -106,28 +121,34 @@ export function handleLogRepay(event: LogRepay): void {
 }
 
 export function handleLogWithdrawFees(event: LogWithdrawFees): void {
-  let cauldronFee = getCauldronFee(event.address)
-  let eventWithdrawFeePart = event.params.feesEarnedFraction.divDecimal(EIGHTEEN_DECIMALS)
+  const cauldronFee = getCauldronFee(event.address)
+  const eventWithdrawFeePart = event.params.feesEarnedFraction.divDecimal(EIGHTEEN_DECIMALS)
   cauldronFee.accrueInfoFeesWithdrawn = cauldronFee.accrueInfoFeesWithdrawn.plus(eventWithdrawFeePart)
   cauldronFee.accrueInfoFeesEarned = BigDecimal.zero()
   cauldronFee.save()
 }
 
 export function handleLogRemoveCollateral(event: LogRemoveCollateral): void {
-  let invoker = event.transaction.from.toHex().toLowerCase()
-  let user = event.params.from.toHex().toLowerCase()
+  const invoker = event.transaction.from.toHex().toLowerCase()
+  const user = event.params.from.toHex().toLowerCase()
   if (invoker != user) {
-    let txHash = event.transaction.hash.toHex()
-    let userLiquidation = getUserLiquidation(user, txHash)
-    userLiquidation.collateralRemoved = event.params.share.divDecimal(EIGHTEEN_DECIMALS)
+    const txHash = event.transaction.hash.toHex()
+    const userLiquidation = getUserLiquidation(user, txHash)
+    const deployedCauldron = cauldronContract.bind(event.address)
+    const collateral = deployedCauldron.collateral()
+    const deployedErc = erc20.bind(collateral)
+    const collateralRemoved = degenbox.bind(deployedCauldron.bentoBox())
+        .toAmount(collateral, event.params.share, false)
+        .divDecimal(decimals(deployedErc.decimals()))
     userLiquidation.cauldron = event.address.toHex()
+    userLiquidation.collateralRemoved = collateralRemoved
     userLiquidation.save()
   }
 }
 
 export function handleLogExchangeRate(event: LogExchangeRate): void {
-  let cauldron = event.address.toHex()
-  let exchangeRate = getExchangeRate(cauldron)
+  const cauldron = event.address.toHex()
+  const exchangeRate = getExchangeRate(cauldron)
   exchangeRate.rate = event.params.rate.divDecimal(EIGHTEEN_DECIMALS)
   exchangeRate.save()
 }
--- src/degenbox.ts
+++ src/degenbox.ts
@@ -1,54 +1,16 @@
 import { LogDeploy } from '../generated/degenbox/degenbox'
 import { cauldron } from '../generated/templates'
 
-// watching bento box for master contract deployments
+// watching bentobox for contracts deployed by abra
+const ABRA_ACCOUNTS = [
+    '0xfddfe525054efaad204600d00ca86adb1cc2ea8a'.toLowerCase(),
+    '0xb4EfdA6DAf5ef75D08869A0f9C0213278fb43b6C'.toLowerCase()
+]
 
-export function handleLogDeployMainnet(event: LogDeploy): void {
-    let masterContracts = [
-        '0x63905bb681b9e68682f392df2b22b7170f78d300'.toUpperCase(), // CauldronV2Flat
-        '0x476b1E35DDE474cB9Aa1f6B85c9Cc589BFa85c1F'.toUpperCase(), // CauldronV2
-        '0x1df188958a8674b5177f77667b8d173c3cdd9e51'.toUpperCase(), // CauldronV2CheckpointV1
-        '0x4a9cb5d0b755275fd188f87c0a8df531b0c7c7d2'.toUpperCase(), // CauldronMediumRiskV1
-        '0x469a991a6bb8cbbfee42e7ab846edeef1bc0b3d3'.toUpperCase(), // CauldronLowRiskV1
-    ]
-    if (masterContracts.indexOf(event.params.masterContract.toHex().toUpperCase()) > -1) {
-        cauldron.create(event.params.cloneAddress)
-    }
-}
+export function handleLogDeploy(event: LogDeploy): void {
+    const account = event.transaction.from.toHex().toLowerCase()
 
-export function handleLogDeployFantom(event: LogDeploy): void {
-    let masterContracts = [
-        '0xe802823719f9d2520415854e6f95bae498ff1d52'.toUpperCase(), // CauldronV2FTM
-    ]
-    if (masterContracts.indexOf(event.params.masterContract.toHex().toUpperCase()) > -1) {
-        cauldron.create(event.params.cloneAddress)
-    }
-}
-
-export function handleLogDeployArbitrum(event: LogDeploy): void {
-    let cauldronContracts = [
-        '0xC89958B03A55B5de2221aCB25B58B89A000215E6'.toUpperCase(), // weth cauldron
-    ]
-    if (cauldronContracts.indexOf(event.params.cloneAddress.toHex().toUpperCase()) > -1) {
-        cauldron.create(event.params.cloneAddress)
-    }
-}
-
-export function handleLogDeployAvalanche(event: LogDeploy): void {
-    let masterContracts = [
-        '0xc568a699c5b43a0f1ae40d3254ee641cb86559f4'.toUpperCase(), // CauldronV2Multichain
-        '0x02e07b6f27e5ec37ca6e9f846b6d48704031625a'.toUpperCase(), // CauldronV2Multichain
-    ]
-    if (masterContracts.indexOf(event.params.masterContract.toHex().toUpperCase()) > -1) {
-        cauldron.create(event.params.cloneAddress)
-    }
-}
-
-export function handleLogDeployBsc(event: LogDeploy): void {
-    let masterContracts = [
-        '0x26fa3fffb6efe8c1e69103acb4044c26b9a106a9'.toUpperCase(), // CauldronV2Multichain
-    ]
-    if (masterContracts.indexOf(event.params.masterContract.toHex().toUpperCase()) > -1) {
+    if (ABRA_ACCOUNTS.indexOf(account) > -1) {
         cauldron.create(event.params.cloneAddress)
     }
 }
